cfffca8e66ce980ca212232006e93f17b4e65b67
Philippe Muller <philippe.muller@gmail.com>
2013-10-26 23:25:41 +0200

    New formula: freetype

---
+++ freetype/freetype-2.5.0.1-1.py
@@ -0,0 +1,8 @@
+class Freetype(Formula):
+
+    name = 'freetype'
+    version = '2.5.0.1'
+    revision = 1
+    sources = File('http://downloads.sf.net/project/freetype/freetype2/2.5.0/freetype-2.5.0.1.tar.gz',
+                   sha1='2d539b375688466a8e7dcc4260ab21003faab08c')
+    dependencies = ('libpng',)
