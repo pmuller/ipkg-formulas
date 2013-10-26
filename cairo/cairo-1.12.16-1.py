ec03e36141a557cbaedcee4f20f74c2f8c303e37
Philippe Muller <philippe.muller@gmail.com>
2013-10-26 22:59:21 +0200

    New formula: cairo

---
+++ cairo/cairo-1.12.16-1.py
@@ -0,0 +1,8 @@
+class Cairo(Formula):
+
+    name = 'cairo'
+    version = '1.12.16'
+    revision = 1
+    sources = File('http://cairographics.org/releases/cairo-1.12.16.tar.xz',
+                   sha256='2505959eb3f1de3e1841023b61585bfd35684b9733c7b6a3643f4f4cbde6d846')
+    dependencies = ('pkg-config', 'pixman', 'xz', 'freetype', 'glib')
