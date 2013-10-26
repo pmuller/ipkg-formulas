1df71edee56127aea6ad2a6758dd07c72e1f61fb
Philippe Muller <philippe.muller@gmail.com>
2013-10-26 17:51:53 +0200

    New formula: nginx

---
+++ nginx/nginx-1.4.3-1.py
@@ -0,0 +1,14 @@
+class Nginx(Formula):
+
+    name = 'nginx'
+    version = '1.4.3'
+    revision = 1
+    homepage = 'http://nginx.org/'
+    sources = File('http://nginx.org/download/nginx-1.4.3.tar.gz',
+                   sha1='8d0c34c84ce6dd8ba4442889e8f2599044c90930')
+    dependencies = ('pcre',)
+    configure_args = ('--prefix=%(env_dir)s',
+                      '--with-http_ssl_module',
+                      '--with-pcre',
+                      '--with-ipv6',
+                      '--with-http_gzip_static_module')
