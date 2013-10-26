7992ff67d755ff70ec58fb48f0614a37ccbeb06d
Philippe Muller <philippe.muller@gmail.com>
2013-10-26 17:42:58 +0200

    New formula: pcre 8.33

---
+++ pcre/pcre-8.33-1.py
@@ -0,0 +1,14 @@
+class Pcre(Formula):
+
+    name = 'pcre'
+    version = '8.33'
+    revision = 1
+    homepage = 'http://www.pcre.org/'
+    sources = File('http://downloads.sourceforge.net/project/pcre/pcre/8.33/pcre-8.33.tar.bz2',
+                   sha256='c603957a4966811c04af5f6048c71cfb4966ec93312d7b3118116ed9f3bc0478')
+    configure_args = ('--prefix=%(env_dir)s',
+                      '--enable-utf8',
+                      '--enable-unicode-properties',
+                      '--enable-pcregrep-libz',
+                      '--enable-pcregrep-libbz2',
+                      '--enable-jit')
