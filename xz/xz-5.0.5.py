0e20fa39efc41b9422210be83f5831fe74fe4838
Philippe Muller <philippe.muller@gmail.com>
2013-10-26 22:59:41 +0200

    New formula: xz

---
+++ xz/xz-5.0.5.py
@@ -0,0 +1,8 @@
+class Xz(Formula):
+
+    name = 'xz'
+    version = '5.0.5'
+    revision = 1
+    homepage = 'http://tukaani.org/xz/'
+    sources = File('http://fossies.org/linux/misc/xz-5.0.5.tar.gz',
+                   sha256='5dcffe6a3726d23d1711a65288de2e215b4960da5092248ce63c99d50093b93a')
