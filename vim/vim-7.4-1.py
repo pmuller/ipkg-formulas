766ceecd4c3ceeb088d112b8a709418bfc889cdb
Philippe Muller <philippe.muller@gmail.com>
2013-10-26 18:31:38 +0200

    New formula: vim

---
+++ vim/vim-7.4-1.py
@@ -0,0 +1,17 @@
+class Vim(Formula):
+
+    name = 'vim'
+    version = '7.4'
+    revision = 1
+    homepage = 'http://vim.org'
+    sources = File('http://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2',
+                   md5='607e135c559be642f210094ad023dc65')
+    dependencies = ('python',)
+    configure_args = ('--prefix=%(env_dir)s',
+                      '--enable-pythoninterp',
+                      '--enable-multibyte',
+                      '--with-tlib=ncurses',
+                      '--enable-cscope',
+                      '--with-features=huge',
+                      '--with-compiledby=ipkg')
+    envvars = {'VIMRUNTIME': '%(env_dir)s/share/vim/vim74'}
