551cdd2c8a609e41ea01e07f76cee61550d262d3
Philippe Muller <philippe.muller@gmail.com>
2013-10-26 22:42:06 +0200

    New formula: pixman

---
+++ pixman/pixman-0.30.2-1.py
@@ -0,0 +1,7 @@
+class Pixman(Formula):
+
+    name = 'pixman'
+    version = '0.30.2'
+    revision = 1
+    sources = File('http://cairographics.org/releases/pixman-0.30.2.tar.gz',
+                   sha256='bd988920ccd742310ddf5b363c7b278f11d69a3405a09d542162c84b46bff6e9')
